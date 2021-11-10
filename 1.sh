b1fa7d43f314f63daa66ae2bc6a176c3bb750a47
schr1ger <mihalbl400@gmail.com>
2021-11-10 12:27:20 +0200

    -add repo

--- 1.sh
+++ 1.sh
@@ -19,7 +19,7 @@ rm -R /var/lib/openvpn &> /dev/null
 #    echo "Ansible already installed"
 #fi
 
-git clone https://github.com/it-toppp/doublevpn.git && cd /root/doublevpn/
+git clone https://github.com/mihalbl400/doublevpn.git && cd /root/doublevpn/
 
 ansible-playbook gen_conf.yml
 echo "Please wait..."
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-   wget https://raw.githubusercontent.com/it-toppp/doublevpn/master/1.sh && chmod +x 1.sh && ./1.sh
+   wget https://raw.githubusercontent.com/mihalbl400/doublevpn/master/1.sh && chmod +x 1.sh && ./1.sh
