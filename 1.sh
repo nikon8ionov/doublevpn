9754e3ade63df5a4f3f151537fd914fb61b42574
nikon8ionov <90381890+nikon8ionov@users.noreply.github.com>
2021-12-28 10:16:25 +0300

    edit ansible install

--- 1.sh
+++ 1.sh
@@ -8,30 +8,43 @@ rm -R /root/1.sh &> /dev/null
 rm -R /var/lib/openvpn &> /dev/null
 ###################
 
+
+# Detect OS
+# $os_version variables aren't always in use, but are kept here for convenience
+if grep -qs "ubuntu" /etc/os-release; then
+	os="ubuntu"
+	os_version=$(grep 'VERSION_ID' /etc/os-release | cut -d '"' -f 2 | tr -d '.')
+	group_name="nogroup"
+elif [[ -e /etc/debian_version ]]; then
+	os="debian"
+	os_version=$(grep -oE '[0-9]+' /etc/debian_version | head -1)
+	group_name="nogroup"
+else
+	echo "This installer seems to be running on an unsupported distribution.
+Supported distros are Ubuntu, Debian."
+	exit
+fi
+
+
 # Install ansible #
-#if ! grep -q "ansible/ansible" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
-#    echo "Adding Ansible PPA"
-#    apt-add-repository ppa:ansible/ansible -y
-#fi
-#
-#if ! hash ansible >/dev/null 2>&1; then
-#    echo "Installing Ansible..."
+echo "Installing Ansible..."
+if "$os" == "ubuntu"; then
+  apt update
+  apt install software-properties-common -y
+  add-apt-repository --yes --update ppa:ansible/ansible
+  apt install ansible -y
+elif "$os" == "debian"; then
+  echo "Adding Ansible PPA"
+  UBUNTU_VERSION=$(dpkg --status tzdata|grep Provides|cut -f2 -d'-')
+  echo "deb http://ftp.debian.org/debian $UBUNTU_VERSION-backports main" | tee /etc/apt/sources.list.d/$UBUNTU_VERSION-backports.list
+fi
 
-# UBUNTU_VERSION=$(dpkg --status tzdata|grep Provides|cut -f2 -d'-')
-# echo "deb http://ftp.debian.org/debian $UBUNTU_VERSION-backports main" | tee /etc/apt/sources.list.d/$UBUNTU_VERSION-backports.list
-UBUNTU_VERSION=$(lsb_release -a | grep Codename | cut -f2)
 
-apt-get update
-apt-get install software-properties-common ansible git curl sshpass python-apt -y
-
-#else
-#    echo "Ansible already installed"
-#fi
-
+apt-get install git curl sshpass python-apt -y
 git clone https://github.com/nikon8ionov/doublevpn.git && cd /root/doublevpn/
 
 
-
+# run Ansible playbook
 ansible-playbook gen_conf.yml
 echo "Please wait..."
 ansible-playbook main.yml
@@ -39,7 +52,8 @@ ansible-playbook main.yml
 CNF=$(cat  /root/doublevpn/wg-client.conf);
 MYIP=$(curl -4 https://icanhazip.com/);
 
-#docker
+
+# docker
 curl -fsSL https://get.docker.com/ | sh
 systemctl start docker
 systemctl enable docker
